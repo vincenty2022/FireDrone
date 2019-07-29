ce286f91e7678a519cb2ae253bf2caada8ff6c5f
[vincenty2022] <[vincenty@princeton.edu]>
2019-07-28 23:12:17 -0400

    added the cloud import/export capability
    added continuous reverse_runs through control_panel.py
    
    reorganized file locations, updated the readme, deleted unneccessary/test files

---
+++ control_panel.py
@@ -0,0 +1,171 @@
+from azure.storage.blob import BlockBlobService
+
+import reverse_run
+import direct_run
+from droneClass import Drone
+
+import keyboard
+import time
+import os
+import json
+
+# FireDrone API key
+api_key = 'cyg-yPk*NBPKa!?%F73$$&8a6y7viE*d8_j$uYL2qnsgEndnWWz^q*zh!FO-d!jJ'
+
+# Azure blob storage info
+account_name = 'firedronestor'
+account_key = 'dEvAqLZ2iU3WbvjehqVb/6d44hhMf5RpJlTpXXEdEeaR6bQtIeOJ6QWxmPjosJhsXdYLw22x/HeHfZi0v1H3Aw=='
+
+continuous_run_time_interval = 60                       # how often to run a reverse_run
+
+def continuous_reverse_run():
+    run = Drone(api_key)
+    print(run.scenes)
+    scene_num = input("Select a scene. Press 'q' to quit: ")
+
+    start = time.time()
+
+    while run.run_id == -1:
+        run.startRun(scene_num)
+
+        if run.run_id != -1:
+            break
+        elif scene_num == 'q':
+            print('Cancelling')
+            return
+
+        scene_num = input("Select a scene. Press 'q' to quit: ")
+
+    reverse_run.reverse_run(run)
+    run.endRun()
+
+    print("Press 'q' to quit")
+
+    while True:
+        wait = True
+        stop = False
+
+        # Guaranteed 5 seconds to cancel
+        if wait:
+            wait_start = time.time()
+            while (time.time() - wait_start < 5):
+                if keyboard.is_pressed('q'):
+                    print('Quitting')
+                    stop = True
+                    break
+
+        if stop:
+            break
+
+        now = time.time()
+        delay = now - start
+
+        if delay >= continuous_run_time_interval:
+            start = now
+
+            run = Drone(api_key)
+            run.startRun(scene_num)
+            reverse_run.reverse_run(run)
+            run.endRun()
+
+            wait = True
+
+            print("Press 'q' to quit")
+
+def direct_run_by_container(droneInstance, blobservice, container_name):
+    # data processing
+
+    blobservice.get_blob_to_path(container_name, 'drone_coordinates.json', os.path.join(os.getcwd(), 'resources\coordinates.json'))
+    scene_num = container_name.split('-')[1]
+    scene_num = int(scene_num)
+
+    droneInstance.startRun(scene_num)
+
+    with open(r'resources\coordinates.json') as f:
+        data = json.load(f)
+
+    coords = []
+    # reading from json
+    for i in data['fires']:
+        x, y = i['x'], i['y']
+        coords.append((x, y))
+
+    while True:
+        for i in range(len(coords)):
+            x, y = coords[i]
+            print(f'{i + 1}. ({x}, {y})')
+
+        index = input("Select a detected fire to start the direct_run on. Press 'q' to quit: ")
+        try:
+            index = int(index)
+            index -= 1
+            if 0 <= index < len(coords):
+                direct_run.directRun(droneInstance, coords[index][0], coords[index][1])
+            else:
+                print('Invalid selection. Out of range')
+        except ValueError:
+            if index == 'q':
+                break
+            print('Invalid selection')
+
+def input_direct_run():
+    run = Drone(api_key)
+
+    service = BlockBlobService(account_name, account_key)
+    containers = service.list_containers()
+
+    c_list = []
+
+    for c in containers:
+        c_list.append(c)
+
+    if len(c_list) != 0:
+        for i in range(len(c_list)):
+            name = c_list[i].name
+            print(f'{i+1}. {name}')
+
+        print(f'{len(c_list) + 1}. Normal Direct Run')
+
+        # select data of interest
+        while True:
+            index = input("Select the data you want to view. Press 'q' to quit: ")
+            try:
+                index = int(index)
+                index -= 1
+                if 0 <= index < len(c_list):                                    # select container index
+                    container_name = c_list[index].name
+                    direct_run_by_container(run, service, container_name)
+                    break
+                elif index == len(c_list):                                      # select the normal direct run option
+                    print("Enabling normal direct_run")
+                    run.startRun()
+                    direct_run.directRun(run)
+                    break
+                else:                                                           # print and try again
+                    print('Invalid input. Enter a value in the correct range')
+            except ValueError:
+                if index == 'q':                                                # cancel
+                    print('Quitting')
+                    return
+                print('Invalid input. Enter an int')                            # print and try again
+
+    else:                                                                       # there are no options
+        print('No data available. Enabling normal direct_run')
+        run.startRun()
+        direct_run.directRun(run)
+
+    run.endRun()
+
+while True:
+    key = input("Select '1' to enter continuous reverse_run or '2' to enter direct_run. Press 'q' to quit: ")
+    if key == '1':
+        continuous_reverse_run()
+        break
+    elif key == '2':
+        input_direct_run()
+        break
+    elif key == 'q':
+        break
+    else:
+        print('Not a valid input')
+
--- predictor.py
+++ predictor.py
@@ -1,12 +1,14 @@
 from azure.cognitiveservices.vision.customvision.prediction import CustomVisionPredictionClient
-import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from PIL import Image
+from PIL.ImageDraw import Draw
+from PIL import ImageFont
+import math
 import numpy as np
 
-####################### MODEL SPECIFIC VARIABLES ###############################
-prediction_key ='09e3db73317249f78fbbb5ac10261ce8'
+##################### AZURE MODEL SPECIFIC VARIABLES ###########################
+prediction_key = '09e3db73317249f78fbbb5ac10261ce8'
 project_id = '8d0b1bca-87e6-4d23-a346-03ac599454c6'
 publishedName = 'It'
 ENDPOINT = 'https://eastus.api.cognitive.microsoft.com'
@@ -18,20 +20,19 @@ fireThreshold = 0.10
 smokeThreshold = 0.00
 ################################################################################
 
-image_path = './frame.png'
-
 class analyze:
-    def __init__(self):
+    def __init__(self, path_to_image_to_predict):
         self.model = CustomVisionPredictionClient(prediction_key, endpoint=ENDPOINT)
         self.fires = []
         self.smoke = []
+        self.image_path = path_to_image_to_predict
 
-    # updates using the image_path
+    # updates using the self.image_path
     def predictUpdate(self):
         fires = []
         smoke = []
 
-        with open(image_path, mode="rb") as test_data:
+        with open(self.image_path, mode="rb") as test_data:
             results = self.model.detect_image(project_id, publishedName, test_data)
 
         for pred in results.predictions:
@@ -65,20 +66,20 @@ class analyze:
         for inst in self.fires:
             x, y = inst.bounding_box.left, inst.bounding_box.top
             delta_x, delta_y = 0.5 - x, 0.5 - y
-            dist = sqrt(delta_x**2 + delta_y**2)
+            dist = math.sqrt(delta_x**2 + delta_y**2)
 
-            fireDist.append(distSq)
+            fireDist.append(dist)
 
         for inst in self.smoke:
             x, y = inst.bounding_box.left, inst.bounding_box.top
             delta_x, delta_y = 0.5 - x, 0.5 - y
-            dist = sqrt(delta_x**2 + delta_y**2)
+            dist = math.sqrt(delta_x**2 + delta_y**2)
 
             fireDist.append(dist)
 
         return fireDist, smokeDist
 
-    # boxes the object and labels with tag name and probability.
+    # boxes the object and labels with tag name and probability for matplotlib
     def __boxObject(self, obj_list, ax, im_width, im_height):
         for inst in obj_list:
             x, y = inst.bounding_box.left, inst.bounding_box.top
@@ -97,18 +98,84 @@ class analyze:
 
             ax.add_patch(rect)
 
+    # display on matplotlib. Use only with direct run
     def dispPredict(self, ax):
         if self.__areBothEmpty():
             self.predictUpdate()
             if self.__areBothEmpty():
                 return
 
-        im_width, im_height = Image.open(image_path).size
+        im_width, im_height = Image.open(self.image_path).size
 
-        img = np.array(Image.open(image_path), dtype = np.uint8)
+        img = np.array(Image.open(self.image_path), dtype = np.uint8)
 
         self.__boxObject(self.fires, ax, im_width, im_height)
         self.__boxObject(self.smoke, ax, im_width, im_height)
 
         ax.imshow(img)
         plt.pause(1)
+
+    # returns nearest drone coordinates using image coordinates
+    def __img_to_drone_coords(self, im_x, im_y, im_width, im_height):
+        im_x, im_y = int(round(im_x)), int(round(im_y))
+        drone_x = int(round((im_x - 250) / 100))
+        drone_y = int(round((im_height - im_y - 250) / 100))
+
+        max_drone_x = int(round((im_width - 500) / 100))
+        max_drone_y = int(round((im_height - 500) / 100))
+
+        # bounding
+        if drone_x < 0:
+            drone_x = 0
+        elif drone_x > max_drone_x:
+            drone_x = max_drone_x
+
+        if drone_y < 0:
+            drone_y = 0
+        elif drone_y > max_drone_y:
+            drone_y = max_drone_y
+
+        return drone_x, drone_y
+
+    # boxes objects and returns a Draw PIL image and list of object center coordinates
+    def __imgBox(self, obj_list, image, im_width, im_height):
+        im = image.convert('RGBA')
+        drawn = Draw(im)
+        fontsize = im_height * 0.01
+
+        font = ImageFont.truetype('arial.ttf', int(round(fontsize)))
+
+        coords = []
+
+        for inst in obj_list:
+            label = inst.tag_name
+            probability = inst.probability
+
+            x0, y0 = inst.bounding_box.left * im_width, inst.bounding_box.top * im_height
+            x1, y1 = x0 + inst.bounding_box.width * im_width, y0 + inst.bounding_box.height * im_height
+            xC, yC = abs(x0 + x1) / 2, abs(y0 + y1) / 2
+
+            xC, yC = self.__img_to_drone_coords(xC, yC, im_width, im_height)
+
+            coords.append((xC, yC))
+
+            drawn.rectangle([(x0, y0), (x1, y1)], outline=(255, 0, 0, 255))
+            drawn.text((x0, y0), f'{label}: {round(probability, 3)}; ({xC}, {yC})', fill=(0, 0, 0, 255), font= font)
+
+        return im, coords
+
+    def imgPredict(self):
+        if self.__areBothEmpty():
+            self.predictUpdate()
+            if self.__areBothEmpty():
+                return
+
+        img = Image.open(self.image_path)
+        im_width , im_height = img.size
+
+        draw, coordList = self.__imgBox(self.fires, img, im_width, im_height)
+
+        return draw, coordList
+
+
+
